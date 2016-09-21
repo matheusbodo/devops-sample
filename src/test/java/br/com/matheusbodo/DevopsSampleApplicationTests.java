447cf5542ea86200d08907ead7cdc5c106203a6d
Matheus Bodo <mcbodo@gmail.com>
2016-09-21 19:23:50 -0300

    Initial commit

---
+++ src/test/java/br/com/matheusbodo/DevopsSampleApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.matheusbodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
