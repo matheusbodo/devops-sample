59626a59f2c3c64cceb0232140e0eb591d927e33
Matheus Bodo <mcbodo@gmail.com>
2016-09-21 21:04:13 -0300

    Testing sonar quality gates

--- src/main/java/br/com/matheusbodo/controller/HomeController.java
+++ src/main/java/br/com/matheusbodo/controller/HomeController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-	public static final String TESTE = "Teste";
-	
     @RequestMapping("/")
     public String index() {
         return "Bem vindo, Matheus!";
